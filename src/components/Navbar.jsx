9585c10b4071a316d5d965cfb05ac3a3a02fab16
m3marlen <marlenarzymatov87924@gmail.com>
2023-04-26 18:27:36 +0300

    second commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -56,7 +56,7 @@ function Navbar() {
                 textDecoration: "none",
               }}
             >
-              Home Page
+              HomePage
             </Typography>
           </Link>
 
